2a6f500f2b46f766d09ba3eb2e7228af936d34d9
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2020-02-13 20:34:35 +0100

    update template

--- template/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-const int N=2e5+5;
-int main()
-{
-
-    return 0;
-}
---
+++ template/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int N=2e5+5;
+int main(){
+    //freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
