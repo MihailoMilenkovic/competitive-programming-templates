6e3cfe2ef21706995411508179ab2da23b3969de
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2020-03-01 20:50:08 +0100

    dodata verzija segmentnog stabla sa raznim query-jima i poboljsani templatovi

--- template/template.cpp
+++ template/template.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define D(x) cerr << #x << " is " << (x) << "\n";
+#define ld long double
 using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 const int N=2e5+5;
+
+
 int main(){
     //freopen("in.txt","r",stdin);
 	//freopen("out.txt","w",stdout);
