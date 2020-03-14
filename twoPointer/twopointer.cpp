058e994c5a0bcb92e51ad98b06a52a70563c0826
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2020-03-14 09:29:17 +0100

    dodat template za two pointer algoritam

---
+++ twoPointer/twopointer.cpp
@@ -0,0 +1,52 @@
+        #include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pii pair<int,int>
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define D(x) cerr << #x << " is " << (x) << "\n";
+#define ld long double
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int N=1000001,L=26;
+int n,a[N],b[N],ans;
+template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) { os << '{' << a.f << ", " << a.s << '}'; return os; }
+template<class T> ostream& operator<<(ostream& os, const vector<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
+template<class T> ostream& operator<<(ostream& os, const deque<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
+template<class T> ostream& operator<<(ostream& os, const set<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const set<T,greater<T> >& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const multiset<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const multiset<T,greater<T> >& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T1,class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+
+int main(){
+    //freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+	}
+	for(int bit=1;bit<(1<<L);bit*=2){
+		int s=0;
+		for(int i=1;i<=n;i++){
+			if(a[i]&bit){
+				s^=bit;
+			}
+			b[i]=a[i]&(bit-1);
+		}
+		ans^=s-(n&1)*s,s=0;
+		sort(b+1,b+n+1);
+		for(int l=1,r=n;l<=n;l++,r=max(l,r)){
+			while(r>l&&b[l]+b[r]>=bit)r--;
+			s^=(n-r);
+		}
+		ans^=(s&1)*bit;
+	}
+	printf("%d\n",ans);
+    return 0;
+}
