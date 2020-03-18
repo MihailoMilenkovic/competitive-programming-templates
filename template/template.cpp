a9a0ea0865410769280344046c0633fa003f52ad
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2020-03-18 18:15:41 +0100

    izmenjen template

--- template/template.cpp
+++ template/template.cpp
@@ -11,7 +11,7 @@
 #define ld long double
 using namespace std;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-const int N=2e5+5;
+const int N=2e5+5,oo=1e9+1e8+7,mod=1e9+7;
 
 template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) { os << '{' << a.f << ", " << a.s << '}'; return os; }
 template<class T> ostream& operator<<(ostream& os, const vector<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
