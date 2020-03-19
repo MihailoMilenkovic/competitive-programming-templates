391f5d1b4d47051b46ec35e152319e67038ff8e3
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2020-03-19 09:28:48 +0100

    dodat template za kmp (pattern matching za stringove)

---
+++ stringovi/kmp.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pii pair<int,int>
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define D(x) cerr << #x << " is " << (x) << "\n";
+#define ld long double
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int N=2e5+5,oo=1e9+1e8+7,mod=1e9+7;
+
+template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) { os << '{' << a.f << ", " << a.s << '}'; return os; }
+template<class T> ostream& operator<<(ostream& os, const vector<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
+template<class T> ostream& operator<<(ostream& os, const deque<T>& a){os << '{';for(int i=0;i<sz(a);i++){if(i>0&&i<sz(a))os << ", ";os << a[i];}os<<'}';return os;}
+template<class T> ostream& operator<<(ostream& os, const set<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const set<T,greater<T> >& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const multiset<T>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T> ostream& operator<<(ostream& os, const multiset<T,greater<T> >& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+template<class T1,class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {os << '{';int i=0;for(auto p:a){if(i>0&&i<sz(a))os << ", ";os << p;i++;}os << '}';return os;}
+vector<int> prefixFunction(string s){
+    int n=s.size();
+    vector<int>pi(n);
+    for(int i=1;i<n;i++){
+        int j=pi[i-1];
+        while(j>0&&s[i]!=s[j]){
+            j=pi[j-1];
+        }
+        if(s[i]==s[j])j++;
+        pi[i]=j;
+    }
+    return pi;
+}
+int kmp(string pat,string text){
+    string nw=pat+"#"+text;
+    int matches=0;
+    vector<int>pref=prefixFunction(nw);
+    for(int i=pat.size()+1;i<=nw.size();i++){
+        if(pref[i]==pat.size()){
+            //cout<<"match on position "<<i<<"\n";
+            matches++;
+        }
+    }
+    return matches;
+}
+int main(){
+    //freopen("in.txt","r",stdin);
+	//freopen("out.txt","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string pattern,text;
+    cin>>pattern>>text;
+    //cout<<prefixFunction(pattern+"#"+text)<<"\n";
+    int numOccurrences=kmp(pattern,text);
+    cout<<numOccurrences<<"\n";
+    return 0;
+}
+/*
+aba abababa
+a abababa 
+*/
